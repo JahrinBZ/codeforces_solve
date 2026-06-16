74102fa2bf1eb8a6676e277af54c690ed16c2740
Jahrin Binte Zahid <155414933+JahrinBZ@users.noreply.github.com>
2026-06-17 01:49:09 +0600

    basic cin cout

---
+++ basic_data_types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+long long l;
+char c;
+float f;
+double d;
+cin >> n >> l >> c >> f >> d;
+cout << n << endl;
+cout << l << endl;
+cout << c << endl;
+cout << f << endl;
+cout << d << endl;
+}
