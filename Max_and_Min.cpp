25b57a5ecb56ca3e0a4c5479c9b2204dbb009594
Jahrin Binte Zahid <155414933+JahrinBZ@users.noreply.github.com>
2026-06-19 00:07:19 +0600

    solved with if else condition

---
+++ Max_and_Min.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+long long A,B,C;
+cin >> A >> B >> C;
+
+{
+ if ( A<=B && A<=C)
+ cout << A << " ";
+ else if ( B<=A && B<=C )
+ cout << B << " ";
+ else if( C<=A && C<=B)
+ cout << C << " ";
+}
+
+
+{
+ if ( A>=B && A>=C)
+ cout << A;
+ else if ( B>=A && B>=C )
+ cout << B;
+ else if( C>=A && C>=B)
+ cout << C;
+}
+
+
+
+}
